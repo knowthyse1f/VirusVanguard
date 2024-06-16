de635af0d5109a01cfb728a918d7a0b674c91cbb
tajreeee <jannatultajree02@gmail.com>
2024-06-16 15:11:40 +0600

    wander

---
+++ src/ai/state/Wander.java
@@ -0,0 +1,17 @@
+package ai.state;
+
+import ai.AiTransition;
+import entity.NPC;
+import game.state.State;
+
+public class Wander extends AiState{
+    @Override
+    protected AiTransition initializeTransition() {
+        return null;
+    }
+
+    @Override
+    public void update(State state, NPC currentCharecter) {
+
+    }
+}
