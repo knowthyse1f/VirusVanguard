16fc50fac36704b940e6612221051bae97156ea0
safusust <safusust@gmail.com>
2024-09-06 14:53:40 +0600

    Testing in Laptop

--- src/Launcher.java
+++ src/Launcher.java
@@ -3,9 +3,6 @@ import game.GameLoop;
 
 public class Launcher {
     public static void main(String[] args){
-        new Thread(new GameLoop(new Game(800,600))).start();}
-
-    //check
-
+        new Thread(new GameLoop(new Game(800,600))).start
 }
 
